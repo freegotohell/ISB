6831f6f2e2eb122de2452da6ed2e8bbda81c9db0
freegotohell <90061474+freegotohell@users.noreply.github.com>
2025-03-16 17:56:29 +0400

    combined encryption into one func

--- lab_1/task1/encrypt.py
+++ lab_1/task1/encrypt.py
@@ -1,19 +1,24 @@
 import json
 from const import ALPHABET
 
-
 CURRENT_INDEX = 0
 
 with open('plane_text.txt', 'r', encoding='utf-8') as f:
-    plane_text = f.read().strip()
+    phrase = f.read().strip()
 
 with open('key.json', 'r', encoding='utf-8') as f:
     hash_key = json.load(f)['key']
 
 
-def get_new_symbol(old, hashed_symbol):
+def encrypt_symbol(plane_text):
+    global CURRENT_INDEX
+    if CURRENT_INDEX + 1 >= len(hash_key):
+        CURRENT_INDEX = 0
+    hashed_symbol = hash_key[CURRENT_INDEX]
+    CURRENT_INDEX += 1
+
     try:
-        current_index = ALPHABET.index(old.lower())
+        current_index = ALPHABET.index(plane_text.lower())
         hash_index = ALPHABET.index(hashed_symbol.lower())
         if current_index + hash_index >= len(ALPHABET):
             new_index = current_index + hash_index - len(ALPHABET)
@@ -21,23 +26,14 @@ def get_new_symbol(old, hashed_symbol):
             new_index = current_index + hash_index
         return ALPHABET[new_index]
     except ValueError:
-        return old
+        return plane_text
 
 
-def get_hash_symbol():
-    global CURRENT_INDEX
-    if CURRENT_INDEX + 1 >= len(hash_key):
-        CURRENT_INDEX = 0
-    encrypted_s = hash_key[CURRENT_INDEX]
-    CURRENT_INDEX += 1
-    return encrypted_s
-
-
-encrypted = ""
-for symbol in plane_text:
-    encrypted += get_new_symbol(symbol, get_hash_symbol())
+encrypted_txt = ""
+for symbol in phrase:
+    encrypted_txt += encrypt_symbol(symbol)
 
 with open('encrypted.txt', 'w', encoding='utf-8') as f:
-    f.write(encrypted)
+    f.write(encrypted_txt)
 
-print(f"Result: {encrypted}")
+print(f"result: {encrypted_txt}")
