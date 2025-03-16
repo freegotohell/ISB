375ad269b414bc4d0b5241ff579a49af0e013dee
freegotohell <90061474+freegotohell@users.noreply.github.com>
2025-03-17 00:22:23 +0400

    added fuct that counts frequency

---
+++ lab_1/task2/decrypt.py
@@ -0,0 +1,31 @@
+import json
+from const import FREQUENCY
+
+
+def read_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read().strip()
+
+
+def get_frequency(text):
+    frequency_dict = {}
+    for i in set(text):
+        frequency_dict[i] = text.count(i) / len(text)
+    return dict(sorted(frequency_dict.items(), key=lambda item: item[1], reverse=True))
+
+
+def main():
+    filename = 'encrypted.txt'
+    encrypted_text = read_file(filename)
+
+    if encrypted_text:
+        freq_dict = get_frequency(encrypted_text)
+
+        with open('frequency.json', 'w', encoding='utf-8') as file:
+            json.dump(freq_dict, file, ensure_ascii=False)
+
+        print(freq_dict)
+
+
+if __name__ == "__main__":
+    main()
